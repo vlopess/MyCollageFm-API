b5701d3d883d6dcded38559d12fda257baaa0bb4
victor <victorlopes9291@gmail.com>
2023-12-29 17:39:11 -0300

    #5 [REF] - Ajustes no path file

--- business.py
+++ business.py
@@ -9,41 +9,54 @@ from cell import Cell
 
 from model import Model
 
-def downloadImg(model: Model):
+#def downloadImg(model: Model):
+def downloadImg():
     c = 0
-    urlList = model.cellList
-    for j in range(0, len(urlList)):
-        image_url = urlList[j].urlImage
+    #urlList = model.cellList
+    cwd = os.getcwd()
+    partialpath = '{}/{}'.format(cwd, 'temporary')
+    for j in range(0, 25):
+        #image_url = urlList[j].urlImage
+        image_url = 'https://lastfm.freetls.fastly.net/i/u/300x300/2a96cbd8b46e442fc41c2b86b821562f.png'
         img_data = requests.get(image_url).content
-        with open('temporary/{}_{}.jpg'.format(model.id,c), 'wb') as handler:
+        path = partialpath + '/{}_{}.jpg'.format('teste',c)
+        with open(path, 'wb') as handler:
             handler.write(img_data)
         c = c + 1
-    
-def getFontSize(lenght, size):
-    x = 2 if size == 240 else 4.5 if size == 300 else 8
-    if(lenght < 33): 
-        return x + 14
-    if(lenght < 45): 
-        return x + 12
-    if(lenght < 55): 
-        return x + 10
-    if(lenght < 65): 
-        return x + 8       
-    return x + 6
 
-def createImg(model: Model):
-    size = getSize(model.size)
-    new = Image.new("RGBA", (1200,1200))
+def getFontSize(lenght, size):
+    x = 0 if size == 240 else 2 if size == 300 else 5
+    if(lenght < 33):
+        return x + 14
+    if(lenght < 45):
+        return x + 12
+    if(lenght < 55):
+        return x + 10
+    if(lenght < 65):
+        return x + 8
+    if(lenght < 75):
+        return x + 7.5
+    return x + 4
+
+#def createImg(model: Model):
+def createImg():
+    size = getSize(3)
+    new = Image.new("RGB", (1200,1200))
     c = 0
+    cwd = os.getcwd()
+    partialpath = '{}/{}'.format(cwd, 'temporary')
     for i in range(0,1200,size):
         for j in range(0,1200,size):
-            img = Image.open("temporary/{}_{}.jpg".format(model.id, c))
+            path = partialpath + '/{}_{}.jpg'.format('teste', c)
+            img = Image.open(path)
             img = img.resize((size,size))
             I1 = ImageDraw.Draw(img)
             position = ((size/2) - 30, size - 35)
-            text = model.cellList[c].name
+            #text = model.cellList[c].name   
+            text = 'engenheiros do hawaii - toda forma de poder'
             c = c + 1
             lenghtText = len(text)
+            print(lenghtText)
             pixels = (10 * len(text))
             x = (size - pixels) / 2
             position = (0, size - 35)
@@ -52,10 +65,11 @@ def createImg(model: Model):
             font = ImageFont.truetype('fonts/Barlow-Light.ttf',fontSize)
             left, top, right, bottom  = I1.textbbox(position,text,font=font)
             I1.rectangle((left-5, top-5, right+5, bottom+5), fill="black")
-            I1.text(position, text, font=font,fill =(255, 255, 255),align="center"),             
-            new.paste(img, (j,i))            
-    filename = "temporary/{}_collage.pdf".format(model.id)
+            I1.text(position, text, font=font,fill =(255, 255, 255),align="center"),
+            new.paste(img, (j,i))
+    filename = partialpath + "/{}_collage.pdf".format('teste')
     new.save(filename)
+    new.show()
     return filename
 
 def getSize(size):
@@ -65,13 +79,13 @@ def getSize(size):
 
 def deleteFilesRequest(id : int):
     cwd = os.getcwd()
-    path = '{}/{}'.format(cwd, 'temporary')
+    path = '{}/mysite/{}'.format(cwd, 'temporary')
     arr = os.listdir(path)
     for file in arr:
         fileID = file.split('_')
         if id == fileID[0]:
             filePath = '{}/{}'.format(path, file)
             os.remove(filePath)
-        
 
+createImg()
 
